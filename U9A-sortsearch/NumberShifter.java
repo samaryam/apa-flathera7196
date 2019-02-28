cd8bbb2f40f606a0b950404a6af7dde2fbfeff74
Amy Flather <aflather@roadrunner.com>
2019-02-28 22:48:48 +0000

    NumberShifter

--- U9A-sortsearch/NumberShifter.java
+++ U9A-sortsearch/NumberShifter.java
@@ -12,9 +12,26 @@ public class NumberShifter
 {
 	public static int[] makeLucky7Array( int size)
 	{
-		return null;
+ 	  int[] ray = new int[size];
+          for(int i = 0; i < size; i++)
+          {
+            ray[i] = (int)( Math.random()*10);
+	  }
+	  return ray;
 	}
 	public static void shiftEm(int[] array)
 	{
-	}
+	  int index = 0;
+	  for(int i = 0; i < array.length; i++)
+	  {
+	    int temp;
+	    if(array[i] == 7)
+	    {
+	      temp = array[index];
+	      array[index] = 7;
+	      array[i] = temp;
+	      index++;
+	    }
+	  }
+	}	
 }
--- U9A-sortsearch/NumberShifterRunner.java
+++ U9A-sortsearch/NumberShifterRunner.java
@@ -12,7 +12,19 @@ public class NumberShifterRunner
 {
 	public static void main( String args[] ) throws IOException
 	{
-		//add test cases
+	  //add test cases 	
+	  int[] array1 = NumberShifter.makeLucky7Array(10);
+	  for( int num: array1)
+	  {
+		System.out.print(num + " ");
+	  }
+	  System.out.println("");
+	  NumberShifter.shiftEm(array1);
+	  for(int num: array1)
+	  {
+		System.out.print(num + " ");
+	  }
+	  System.out.println("");
 	}
 }
 
